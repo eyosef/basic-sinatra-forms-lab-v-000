4b0d5bc4b876b3ba04f4da7c35b46ef480e7d2d4
Eden Mesfun <emesfun@gmail.com>
2018-03-21 20:55:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @"team_list = ["Team Name: #{params[:name]}","Coach: #{params[:coach]}","Point Guard: #{params[:pg]}",
+    @team_list = ["Team Name: #{params[:name]}","Coach: #{params[:coach]}","Point Guard: #{params[:pg]}",
     "Shooting Guard: #{params[:sg]}", "Power Forward: #{params[:pf]}", "Small Forward: #{params[:sf]}",
     "Center: #{params[:c]}"]
 
