e8fc3cb29713b88ad3062b351e257a1b6c8b47a0
Eden Mesfun <emesfun@gmail.com>
2018-03-21 20:55:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,16 +10,6 @@ class App < Sinatra::Base
     "Shooting Guard: #{params[:sg]}", "Power Forward: #{params[:pf]}", "Small Forward: #{params[:sf]}",
     "Center: #{params[:c]}"]
 
-    team_list = <<-FOO
-    Team Name: #{params[:name]}
-    Coach: #{params[:coach]}
-    Point Guard: #{params[:pg]}
-    Shooting Guard: #{params[:sg]}
-    Power Forward: #{params[:pf]}
-    Small Forward: #{params[:sf]}
-    Center: #{params[:c]}
-    FOO
-
     erb :team
 
   end
